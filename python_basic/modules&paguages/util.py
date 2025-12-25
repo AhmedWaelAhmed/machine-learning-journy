b4b7ff2f415b20b76d6441064d9977e646d6d9eb
Ahmed Khalifa <koremwael@gmail.com>
2025-12-25 08:57:36 +0200

    modules

---
+++ python_basic/modules&paguages/util.py
@@ -0,0 +1,14 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+class car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+names=["Alice", "Bob", "Charlie"]
+print(names)
+
+
+if __name__ == "__main__":
+    print("2. هذا السطر يُطبع فقط عند تشغيل tools مباشرة")
